24d8773bcd36879cbe5e3048cde74fd425cf6aec
Dino <dinokaric99@gmail.com>
2025-01-20 15:29:47 -0500

    Testing stuff in C.

---
+++ Comments_and_escapes.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    // This is a commment
+    /*
+    This is a multi-line comment
+    */
+   printf("Pizza tastes great!\n");
+   printf("1\t2\t3\n4\n5\n6\n");
+   printf("\"I love pizza\" - Abraham Lincoln probably.");
+   return 0;
+}
